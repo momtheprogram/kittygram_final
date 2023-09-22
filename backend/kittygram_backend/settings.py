74d8a920ba037c3b34e097d4cb867394bf658e14
momtheprogram <momtheprogram@gmail.com>
2023-09-22 10:54:27 +0300

    cfg

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -96,7 +96,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
