8909f523e11e976c4797cf1bd464ea3b2c693b1b
momtheprogram <momtheprogram@gmail.com>
2023-09-22 17:07:32 +0300

    toml

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -6,7 +6,6 @@ from rest_framework import routers
 
 from cats.views import AchievementViewSet, CatViewSet
 
-
 router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
 router.register(r'achievements', AchievementViewSet)
